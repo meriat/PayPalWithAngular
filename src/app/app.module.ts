0e4b13d67ea16c7247774cd934169098840458f9
Meria Thomas <josemeria93@gmail.com>
2018-10-26 15:37:28 -0700

    generate timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BodySecondaryComponent } from './body-secondary/body-secondary.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    BodySecondaryComponent
+    BodySecondaryComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
