992616b386a5aab431c2b9a90dcc24689cb998f5
Meria Thomas <josemeria93@gmail.com>
2018-10-31 17:56:54 -0700

    connect firebase database to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,16 +12,26 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BodySecondaryComponent } from './body-secondary/body-secondary.component';
 import { TimelineComponent } from './timeline/timeline.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormsModule,
     NavBarComponent,
     BodySecondaryComponent,
     TimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
