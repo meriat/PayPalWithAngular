417659e940eff49a9e71ffd6bae37d07ccef2ae4
Meria Thomas <josemeria93@gmail.com>
2018-11-02 15:04:23 -0700

    make user nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TimelineComponent } from './timeline/timeline.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AdminComponent } from './admin/admin.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { UserNavComponent } from './user-nav/user-nav.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -33,7 +34,8 @@ export const firebaseConfig = {
     TimelineComponent,
     WelcomeComponent,
     AdminComponent,
-    UserPageComponent
+    UserPageComponent,
+    UserNavComponent
   ],
   imports: [
     BrowserModule,
