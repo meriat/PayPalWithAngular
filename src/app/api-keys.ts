bf522adaae274314afdfd0cf610e7899d0dd9f9c
Meria Thomas <josemeria93@gmail.com>
2018-11-09 11:17:44 -0800

    remove api-keys from .gitignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyBPfGEtYvMgAHouTCtrj8fHGE9eCwAA69k",
+    authDomain: "rebuild-3fdf1.firebaseapp.com",
+    databaseURL: "https://rebuild-3fdf1.firebaseio.com",
+    projectId: "rebuild-3fdf1",
+    storageBucket: "rebuild-3fdf1.appspot.com",
+    messagingSenderId: "354913065268"
+  };
